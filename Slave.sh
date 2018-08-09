3caded63ca08b6793b59dbe4d93cace44a568dd0
Angelo Hernandez <angelo.hernandez@cloudhesive.com>
2018-08-09 16:10:32 -0400

    Update
    Update

---
+++ Slave.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Install AWSCLI Bundle
+cd ~/
+mkdir AWSCliBundle
+cd AWSCliBundle
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+#Install Java 8
+cd ~/
+sudo yum install -y java-1.8.0-openjdk.x86_64 || true \n
+sudo /usr/sbin/alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java || true\n
+sudo /usr/sbin/alternatives --set javac /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/javac || true \n
+sudo yum remove java-1.7 || true \n
+
+#Install Packer
+cd ~/
+mkdir packer
+cd packer
+wget https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip
+unzip -o packer_1.2.5_linux_amd64.zip
+
+#Install Terraform
+cd ~/
+mkdir terraform
+cd terraform
+wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
+unzip -o terraform_0.11.7_linux_amd64.zip
+
+#Install Git
+sudo yum install git -y
+
+#Install Ansible
+sudo amazon-linux-extras install ansible2 -y
